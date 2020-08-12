336240f3d500c84a1ea3c3df0f98c137430ec005
Vo Duy That <thatvd@rakumo.vn>
2020-08-12 13:48:41 +0700

    Updates

---
+++ precache-manifest.6f64d6d7687ca817bb2f66f21b400d91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1108a6d3f5c383963e37a82bbf724fb8",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "5093c5056752d3d9604b",
+    "url": "/demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ce94605fe9e6d52745e",
+    "url": "/demo/static/js/2.02d832a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo/static/js/2.02d832a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5093c5056752d3d9604b",
+    "url": "/demo/static/js/main.6e8b02cd.chunk.js"
+  },
+  {
+    "revision": "30c197e06ef64cd06220",
+    "url": "/demo/static/js/runtime-main.50c93e66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
